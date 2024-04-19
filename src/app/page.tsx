2eef93b3a90a770a72d80f4f674e66f39f599f01
Maicol Santos <maicolcostaa8@gmail.com>
2024-04-19 21:58:30 +0100

    Improve layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import { AnswerItem } from '@/components/answerItem'
 import { Question } from '@/components/question'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { MessageCircleQuestion } from 'lucide-react'
 
 export default function Home() {
   return (
     <div className="max-w-5xl mx-auto p-10">
-      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight mb-4">
-        Trivia English
-      </h1>
-
-      <Question />
-      <AnswerItem />
+      <div className="flex items-end gap-2 mb-8">
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
+          <MessageCircleQuestion size={40} />
+          Trivia English
+        </h1>
+        <Badge variant="destructive">C1</Badge>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Question />
+        <AnswerItem />
+        <div className="w-full block mt-8">
+          <Button>Check Answer</Button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/question.tsx
+++ src/components/question.tsx
@@ -1,6 +1,6 @@
 export const Question = () => {
   return (
-    <p className="mb-8">
+    <p className="">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, neque
       esse. Incidunt, eaque porro ad fugiat iure velit perferendis dolor tenetur
       ab repellendus. Fugit consequuntur possimus vitae esse enim aut?
