fd937cc0312fad4ce4e3a7edcd78bf9db94e334a
Maicol Santos <maicolcostaa8@gmail.com>
2024-04-21 17:41:10 +0100

    Add info gpt

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,19 @@ import { ToggleTheme } from './toggleTheme'
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between py-4">
-      <h1 className="scroll-m-20 text-4xl font-bold tracking-tight mb-8">
-        <MessageCircleQuestion size={40} />
-        Quiz English
-      </h1>
-      <ToggleTheme />
-    </header>
+    <>
+      <header className="py-4  mb-8">
+        <div className="flex items-center justify-between">
+          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
+            <MessageCircleQuestion size={40} />
+            Quiz English
+          </h1>
+          <ToggleTheme />
+        </div>
+        <small className="text-sm font-medium leading-none text-muted-foreground">
+          All questions were created with chatGPT
+        </small>
+      </header>
+    </>
   )
 }
